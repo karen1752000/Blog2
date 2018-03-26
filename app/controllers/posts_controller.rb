5988dadea76fbe81958f71e658129fcb4277e771
Karen Mitchell <karen1752000@yahoo.com>
2018-03-25 23:06:30 -0400

    created post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,19 @@
 class PostsController < ApplicationController
 	def index
+
+	end
+
+	def new
+	end
+
+	def create
+		#render plain: params[:post].inspect
 		
 	end
+
+
+	# def post_params
+ #  		params.require(:post).permit(:image, :caption)
+ #  		redirect_to posts_path
+	# end
 end
