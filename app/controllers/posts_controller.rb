39912bb86ecc705c71b7a890c476c99c12c6ac0f
Karen Mitchell <karen1752000@yahoo.com>
2018-03-25 23:24:42 -0400

    created post show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
 	def index
+		@posts =Post.all
+	end
 
+	def show
+		@post = Post.find(params[:id])
 	end
 
 	def new
@@ -8,12 +12,14 @@ class PostsController < ApplicationController
 
 	def create
 		#render plain: params[:post].inspect
-		
+		@post = Post.new(post_params)
+
+		@post.save
+		redirect_to @post
 	end
 
 
-	# def post_params
- #  		params.require(:post).permit(:image, :caption)
- #  		redirect_to posts_path
-	# end
+	private def post_params
+		params.require(:post).permit(:title, :body)
+	end
 end
